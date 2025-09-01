e7cdb5e4e0f07f1e6ef1c2750dc006e2c3d34555
DamainBL <dbermudezl@unal.edu.co>
2025-09-01 18:05:38 -0500

    Add files via upload

---
+++ numeros del 5 al 15000.py
@@ -0,0 +1,24 @@
+print ("bienvenido a numeros del 5 al 15000 \n")
+
+menu = 5
+
+numero = 0
+
+while (menu!=0):
+
+    menu = int(input("mini menu--- escribe 1 para realizar el conteo --- escribe 0 para salir --- \n"))
+    
+    if(menu==1):
+
+        for i in range(1, 3001):
+
+            numero = numero + 5
+        
+            print (numero)
+    elif(menu==0):
+
+        print("adios")
+
+    else:
+             
+        print("opcion no encontrada")
