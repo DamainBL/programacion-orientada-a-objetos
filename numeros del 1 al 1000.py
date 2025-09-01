b3b161a1fd42694580fffd86b473b3c5b9e1bd07
DamainBL <dbermudezl@unal.edu.co>
2025-09-01 17:55:02 -0500

    Add files via upload

---
+++ numeros del 1 al 1000.py
@@ -0,0 +1,28 @@
+print ("bienvenido a numeros del 1 al 10000 \n")
+
+menu = 5
+
+numero = 0
+
+while (menu!=0):
+
+    menu = int(input("mini menu--- escribe 1 para realizar el conteo --- escribe 0 para salir --- \n"))
+    
+    if(menu==1):
+
+        for i in range(1, 10001):
+
+            numero = numero + 1
+        
+            print (numero)
+    elif(menu==0):
+
+        print("adios")
+
+    else:
+             
+        print("opcion no encontrada")
+
+
+
+
