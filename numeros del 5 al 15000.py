96093b77872121fd2f61c132a66ba2181c12128d
DamainBL <dbermudezl@unal.edu.co>
2025-09-01 20:02:59 -0500

    Update numeros del 5 al 15000.py

--- numeros del 5 al 15000.py
+++ numeros del 5 al 15000.py
@@ -1,7 +1,9 @@
 #Damian Bermudez Lara
-#calcular area de rectangulo 
+#suma de numeros hasta el 15000
 
 print ("bienvenido a numeros del 5 al 15000 \n")
+
+#declarar variables
 
 menu = 5
 
@@ -9,10 +11,14 @@ numero = 0
 
 while (menu!=0):
 
+    #esperar a order del usuario
+    
     menu = int(input("mini menu--- escribe 1 para realizar el conteo --- escribe 0 para salir --- \n"))
     
     if(menu==1):
 
+    #bucle de suma hasta completar los 15000
+        
         for i in range(1, 3001):
 
             numero = numero + 5
@@ -25,4 +31,5 @@ while (menu!=0):
     else:
              
         print("opcion no encontrada")
+
 
