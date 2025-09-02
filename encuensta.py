47c668a390534616f8ee1432afa5467366c3817a
DamainBL <dbermudezl@unal.edu.co>
2025-09-02 15:45:08 -0500

    Add files via upload

---
+++ encuensta.py
@@ -0,0 +1,21 @@
+#Damian Bermudez LAra
+#encuesta
+
+#obtener nombre, carrera, idea de proyecto
+
+for i in range(0, 5):
+
+    print("\nbienvino a la encuesta del curso de programacion")
+    print("primera pregunta\n \ncomo te llamas?: ")
+    nombre = str(input(" -------- "))
+    print("segunda pregunta\n \ncual es tu carrera?: ")
+    carrera= str(input(" -------- "))
+    print("tercera pregunta\n \ncual es tu idea de proyecto?: ")
+    proyecto = str(input(" -------- "))
+
+    print("---------------------------------------------------")
+    print("nombre: ", nombre)
+    print("carrera: ", carrera)
+    print("proyecto: ", proyecto)
+    print("---------------------------------------------------")
+
