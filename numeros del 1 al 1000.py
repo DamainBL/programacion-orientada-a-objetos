f6088df3c9bc2f9f4f9ede573bc2b6cad0c54822
DamainBL <dbermudezl@unal.edu.co>
2025-09-01 20:01:59 -0500

    Update numeros del 1 al 1000.py

--- numeros del 1 al 1000.py
+++ numeros del 1 al 1000.py
@@ -1,7 +1,9 @@
 #Damian Bermudez Lara
-#calcular area de rectangulo 
+#numeros dell 1 al mil
 
 print ("bienvenido a numeros del 1 al 10000 \n")
+
+#declarar variables
 
 menu = 5
 
@@ -9,15 +11,20 @@ numero = 0
 
 while (menu!=0):
 
+    #esperar a que el usuario de la orden de empezar a contar
+
     menu = int(input("mini menu--- escribe 1 para realizar el conteo --- escribe 0 para salir --- \n"))
     
     if(menu==1):
 
+    #bucle de suma hasta completar  10000
+    
         for i in range(1, 10001):
 
             numero = numero + 1
         
             print (numero)
+            
     elif(menu==0):
 
         print("adios")
@@ -25,6 +32,7 @@ while (menu!=0):
     else:
              
         print("opcion no encontrada")
+
 
 
 
